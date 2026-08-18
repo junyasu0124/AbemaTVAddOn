81723bf78d38827f5ba0b713cd0118c717ea0790
junyasu0124 <jumpeiyasue@gmail.com>
2026-08-18 22:56:12 +0900

    fix: website update support

--- extension/content_script.js
+++ extension/content_script.js
@@ -136,7 +136,7 @@ function saveSettingsInLocalStorage() {
 
 function setRateButtonClickEvent() {
   const id = setInterval(() => {
-    const playerContainer = document.querySelector('.c-tv-TimeshiftPlayerContainerView');
+    const playerContainer = document.querySelector('.c-vod-EpisodePlayerContainer-wrapper');
     const rateButtons = document.querySelectorAll('.com-vod-VODSettingsBlock__check-item');
     if (playerContainer !== null && rateButtons.length > 0) {
       clearInterval(id);
